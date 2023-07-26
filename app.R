dd13cdeb4a28a02e71ecbec6eb01fff4164a3fed
Art Steinmetz <apsteinmetz@yahoo.com>
2023-07-26 15:27:59 -0400

    title bar

--- app.R
+++ app.R
@@ -102,7 +102,7 @@ playlists<-playlists %>%
 
 #  DEFINE USER INTERFACE ===============================================================
 ui <- {
-  navbarPage("WFMU Playlist Explorer BETA VERSION 0.7",theme = shinytheme("darkly"),
+  navbarPage("WFMU Playlist Explorer BETA VERSION 0.8",theme = shinytheme("darkly"),
                  # -- Add Tracking JS File 
                  #rest of UI doesn't initiate unless tab is clicked on if the code below runs
                  #tags$head(includeScript("google-analytics.js"))
